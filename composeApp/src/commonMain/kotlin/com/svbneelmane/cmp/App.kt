381909999e583eaa034bdc16b6665b207b1e80ec
shivaprasad-bhat <shivaprasad@svbneelmane.in>
2025-06-12 22:01:35 +0530

    Commit 4:: Added theme in App()

--- composeApp/src/commonMain/kotlin/com/svbneelmane/cmp/App.kt
+++ composeApp/src/commonMain/kotlin/com/svbneelmane/cmp/App.kt
@@ -1,9 +1,13 @@
 package com.svbneelmane.cmp
 
 import androidx.compose.runtime.Composable
+import com.svbneelmane.cmp.theme.CoinRoutineTheme
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 @Preview
 fun App() {
+    CoinRoutineTheme {
+
+    }
 }
